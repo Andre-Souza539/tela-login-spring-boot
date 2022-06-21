79d719e58f306b035d8605908791242f6386c35a
aljsjunca <aljs.junca@gmail.com>
2022-06-21 15:12:21 -0300

    criado o primeiro sistema de login utilizando httpSecurity

---
+++ login/src/main/java/br/com/desafio/login/SecSecurityConfig.java
@@ -0,0 +1,39 @@
+package br.com.desafio.login;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests()
+				.antMatchers("/home").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.httpBasic();
+				
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user = 
+				User.withDefaultPasswordEncoder()
+					.username("admin")
+					.password("admin")
+					.roles("ADMIN")
+					.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+}
